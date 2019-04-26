4611973ac9fa290e84508b040f504fc730dd629a
Chowdhury Ashraf <cmashraf@uw.edu>
2019-04-26 15:45:09 -0700

    unique species

--- kmpy/kmpy.py
+++ kmpy/kmpy.py
@@ -1,4 +1,23 @@
 import ode_builder
 
+#Reading the filepath and storing them
+
 reactionlist, rateconstantlist, compositionlist = ode_builder.set_paths()
-print(reactionlist)
+
+
+#initializing reactant, product and unique species list
+reactants_list = []
+products_list = []
+species_name = []
+
+for line in open(reactionlist, 'r').readlines():
+    reac = ode_builder.Reaction() #Reaction class initiated
+    reactants_list.append(reac.getReactantsName(line))
+    products_list.append(reac.getProductsName(line))
+    current_species = species_name
+    #print(current_species)
+    species_list = reac.uniqueSpeciesName(line, current_species)
+    #print(species_name)
+species_list.sort()
+
+print(species_list)
--- kmpy/ode_builder.py
+++ kmpy/ode_builder.py
@@ -4,6 +4,7 @@ import os
 
 
 def set_paths():
+    
     """
     Set the absolute path to required files on the current machine.
 
@@ -16,9 +17,64 @@ def set_paths():
     compositionlist_path  : str
                             path to the file `compositionlist.dat`
     """
+    
     module_dir = os.getcwd().split('ligpy_utils')[0]
     reactionlist_path = module_dir + '/data/complete_reaction_list.dat'
     rateconstantlist_path = module_dir + '/data/complete_rateconstant_list.dat'
     compositionlist_path = module_dir + '/data/compositionlist.dat'
 
     return reactionlist_path, rateconstantlist_path, compositionlist_path
+
+
+class Reaction(object):
+    
+    """
+    This is reaction class - it reads the reaction file and generate  
+    reactant list, product list and list of unique species in the mechanism.
+    """    
+    
+    def __init__(self):
+
+        #initiating the class
+        self.reactants_names = []
+        self.products_names = []
+        self.uniqueSpeciesList = []
+        #species_names = []
+        
+   
+    def getReactantsName(self, line):
+        
+        #getting the reactants for each reaction
+        for spec in line.split(','):
+            if float(spec.split('_')[0].split()[0]) < 0:
+                self.reactants_names.append((spec.split('_')[0].split()[0],
+                                          spec.split('_')[1].split()[0]))
+            #print(self.species_names)
+        return self.reactants_names
+    
+    def getProductsName(self, line):
+        
+        #getting the reactants for each reaction
+        for spec in line.split(','):
+            if float(spec.split('_')[0].split()[0]) > 0:
+                self.products_names.append((spec.split('_')[0].split()[0],
+                                          spec.split('_')[1].split()[0]))
+            #print(self.species_names)
+        return self.products_names
+    
+    def uniqueSpeciesName(self, line, species_list):
+        
+        #building the unique species list
+        #self.uniqueSpeciesList = species_list
+        for spec in line.split(','):
+            #self.uniqueSpeciesList = species_list
+            # If the species has already been added to the list then move on.
+            if spec.split('_')[1].split()[0] in species_list:
+                self.uniqueSpeciesList = species_list
+                continue
+            else:
+                #print(self.uniqueSpeciesList)
+                self.uniqueSpeciesList = species_list
+                self.uniqueSpeciesList.append(spec.split('_')[1].split()[0])
+            #print(spec.split('_')[1].split()[0])
+        return self.uniqueSpeciesList

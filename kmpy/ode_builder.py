ba1ad4b6a548927a3cb7b7f58e7e3beb2b6d8c4d
Chowdhury Ashraf <cmashraf@uw.edu>
2019-08-05 17:23:54 -0700

    started adding chemkin functionalities

--- kmpy/ode_builder.py
+++ kmpy/ode_builder.py
@@ -4,13 +4,17 @@ from .constants import KCAL_JL, HT_JL
 import math
 import pandas as pd
 import re
+from itertools import islice
 
 
 def set_paths(my_path):
 
     """
     Set the absolute path to required files on the current machine.
-
+    Parameters
+    -------
+    my_path                 : str
+                            path where all the imput files are located
     Returns
     -------
     reactionlist_path     : str
@@ -26,6 +30,199 @@ def set_paths(my_path):
     free_energy_path = my_path + '/data/free_energy_library.dat'
 
     return reactionlist_path, rateconstantlist_path, free_energy_path
+
+
+def set_paths_chemkin_files(my_path):
+    """
+    Set the absolute path to required files on the current machine.
+    *** only required if using chemkin files****
+    Parameters
+    -------
+    my_path                 : str
+                            path where all the imput files are located
+    Returns
+    -------
+    thermo_path            : str
+                            path to the chemkin thermo file
+    smile_path             : str
+                            path to the file `species_smiles.dat`
+    reactionlist_path      : str
+                            path to the chemkin reaction file
+    """
+
+    thermo_path = my_path + '/thermo.dat'
+    smile_path = my_path + '/species_smiles.dat'
+    reactionlist_path = my_path + 'reaction.dat'
+    return thermo_path, smile_path, reactionlist_path
+
+
+def generate_smile_dict_chemkin(path):
+    """
+    creates a dictionary where key in the species formula
+    and the value is the corresponding smiles
+    *** only required if using chemkin files****
+    paratemetrs
+    -------
+    path                   : str
+                            path of the 'species_smiles.dat' file
+    Returns
+    -------
+    dict_smile             : dict
+                            dictionary key = formula, value = smiles
+
+    """
+    data = pd.read_csv(path)
+    dict_smiles = dict(zip(data.Molecule, data.Smile))
+
+    return dict_smiles
+
+
+def parse_chemkin_thermo(file_name, dictionary):
+    """
+    parse the chemkin thermo_file, gets all the
+    coefficient values for all the species
+    Parameters
+    -------
+    file_name               : str
+                            path of the 'thermo.dat' file
+    dictionary              : dict
+                            the smiles dictionary generated from
+    Returns
+    -------
+    low                     : float
+                            low temperature
+    mid                     : float
+                            mid temperature
+    high                    : float
+                            high temperature
+    ***Each chemkin thermo file comes with a low, mid and high temperature
+    values where the thermo coefficients are valid***
+    dict_thermo_coeff       : dict
+                            A dictionary where the key is the smiles of the
+                            molecules and the values are the corresponding
+                            coefficients from the chemkin thermo file
+                            to calculate the thermodynamic properties
+    """
+
+    # number of lines in each chunk
+    num = 4
+    #     file = open(file_name, 'r')
+
+    species_list = []
+    thermo_coeff = []
+
+    with open(file_name) as f:
+        for i in range(2):
+            line = f.readline()
+            if i == 1:
+                low = float(line.split()[0])
+                mid = float(line.split()[1])
+                high = float(line.split()[2])
+
+        while True:
+            next_n_lines = list(islice(f, num))
+            if not next_n_lines:
+                break
+            smi, thermo = collect_thermo_params(next_n_lines, dictionary)
+            species_list.append(smi)
+            thermo_coeff.append(thermo)
+    dict_thermo_coeff = dict(zip(species_list, thermo_coeff))
+    #    print(thermo_dict)
+    return low, mid, high, dict_thermo_coeff
+
+
+def collect_thermo_params(lines, dictionary):
+    """
+    This fucntion generates a dictionary where key is the
+    molecule smiles and values are lists of coefficients
+    from the chemkin thermo file those are required to
+    calculate the thermodynamic properties of each cpecies.
+    Parameters
+    -------
+    lines               : list
+                        chunk of four lines for each species
+                        from chemkin thermo file
+    dictionary          : dict
+                        the smiles dictionary generated from
+                        species_smiles.dat file
+    Returns
+    -------
+    dictionary[species_name]   : str
+                                smiles notation of the species
+    thermo_list             : list
+                            A list of coeffiecents from chemkin
+                            thermo file
+    """
+    thermo_list = []
+    species_name = None
+    for i, line in enumerate(lines):
+        if i == 0:
+            species_name = line.split()[0]
+        else:
+            if i == 3:
+                n = 60
+            else:
+                n = 75
+            for start in range(0, n, 15):
+                s = line[start:start + 15]
+                thermo_list.append(float(s))
+    #    print(thermo_params)
+    #    print(dictionary[species_name])
+    return dictionary[species_name], thermo_list
+
+
+def generate_thermo_dict(file_path, smiles, temp):
+    """
+    This function generates the thrmodynamic properties
+    of all the species present in the machanism from the
+    chemkin thermo file.
+    Parameters
+    -------
+    file_path           : str
+                        path of the chemkin thermo file
+    smiles              : dict
+                        the smiles dictionary generated from
+                        species_smiles.dat file
+    temp                : float
+                        system temperature
+    Returns
+    -------
+    dic_thermo_values   : dict
+                        a dictionary where the keys are the smiles of a
+                        species and the values are a list of speciefic heat,
+                        enthalpy, entropy and free energy of that epecies
+                        at the given temperature
+    """
+    low_t, mid_t, high_t, thermo_params_dict = parse_chemkin_thermo(file_path, smiles)
+    if temp < mid_t:
+        i = 0
+    else:
+        i = 7
+
+    species_names = []
+    thermo_list = []
+    for species, params in thermo_params_dict.items():
+        #        print(species)
+        species_names.append(species)
+        specific_heat = (params[i + 0] + params[i + 1] * temp +
+                         params[i + 2] * temp ** 2 +
+                         params[i + 3] * temp ** 3 +
+                         params[i + 4] * temp ** 4) * GAS_CONST
+        enthalpy = (params[i + 0] + params[i + 1] * temp / 2 +
+                    params[i + 2] * temp ** 2 / 3 +
+                    params[i + 3] * temp ** 3 / 4 +
+                    params[i + 4] * temp ** 4 / 5 +
+                    params[i + 5] / temp) * temp * GAS_CONST
+        entropy = (params[i + 0] * np.log(temp) + params[i + 1] * temp +
+                   params[i + 2] * temp ** 2 / 2 +
+                   params[i + 3] * temp ** 3 / 3 +
+                   params[i + 4] * temp ** 4 / 4 + params[i + 6]) * GAS_CONST
+        free_energy = enthalpy - entropy * temp
+        list_entries = [specific_heat, enthalpy, entropy, free_energy]
+        thermo_list.append(list_entries)
+    #    print(species_names)
+    dict_thermo_values = dict(zip(species_names, thermo_list))
+    return dict_thermo_values
 
 
 class Reaction(object):
@@ -245,12 +442,12 @@ def build_free_energy_dict(free_energy_path, temp):
     else:
         temp_low = math.floor(temp / 100.0) * 100
         temp_high = math.ceil(temp / 100.0) * 100
-        df["{}K".format(temp)] = (df["{}K".format(temp_high)] -
-                                  df["{}K".format(temp_low)]) *\
-                                 (temp - temp_low) / (temp_high - temp_low)\
-                                 + df["{}K".format(temp_low)]
-        df["Free Energy @{}K".format(temp)] = df['electronic_energy'] + \
-                                              df["{}K".format(temp)]
+        df["{}K".format(temp)] = ((df["{}K".format(temp_high)] -
+                                  df["{}K".format(temp_low)]) *
+                                  (temp - temp_low) / (temp_high - temp_low) +
+                                  df["{}K".format(temp_low)])
+        df["Free Energy @{}K".format(temp)] = (df['electronic_energy'] +
+                                               df["{}K".format(temp)])
 # print(df.head())
 
     free_energy = dict([(i, a) for i, a in
@@ -259,7 +456,38 @@ def build_free_energy_dict(free_energy_path, temp):
     return free_energy
 
 
-def build_free_energy_change(complete_list, free_energy):
+def build_stoic_matrix(complete_list, species_list, indices_species):
+    """
+    builds the stoichiometric matrix for the entire mechanism and
+    then builds the rate equations for each reaction.
+    Parameters
+    ----------
+    complete_list    : list
+                      A list of all the reactions with reactant and
+                      product species and their stoichimetric coeffs
+    species_list     : list
+                     A list of unique species in the mechanism
+    indices_species  : dict
+                     the dictionary species_indices
+    Returns
+    ----------
+    matrix           : matrix
+                     stoichiometric matrix for the entire
+                     reaction mechanism
+    rate_final       : str
+                     list of strings of rate equations for each
+                        reaction
+    """
+
+    matrix = np.zeros((len(complete_list), len(species_list)), dtype=float)
+    rate_final = []
+    for rxnindex, reac_list in enumerate(complete_list):
+        rate = build_rate(reac_list, species_list, matrix, rxnindex, indices_species)
+        rate_final.append(rate)
+    return matrix, rate_final
+
+
+def build_free_energy_change(free_energy, species_list, stoic_mat, factor, chemkin=True):
     """
     Calculate the free energy changes for all the reactions
             delG = G(products) - G(reactanat)
@@ -267,13 +495,21 @@ def build_free_energy_change(complete_list, free_energy):
     and free_energy_dict
     Parameters
     ----------
-    complete_list        : list
-                           A list of all the reactions with reactant and
-                         product species and their stoichimetric coeffs
     free_energy          : dict
                           A dictionary of free energies of all the species
                           at a given temperature, obtained from
-                          build_free_energy_dict function
+                          build_free_energy_dict function or from chemkin
+                          thermo files
+    species_list        : list
+                        a list of unique species in the mechanism
+    stoic_mat           : nampy array
+                        stoichiometric matrix of the mechanism
+    factor              : float
+                        conversion factor from given unit of energy to kJ
+    chemkin             : bool
+                        indicates if chemkin files are read as input files
+                        default = True
+
     Returns
     -------
     gibbs_enenrgy_change : list
@@ -282,44 +518,23 @@ def build_free_energy_change(complete_list, free_energy):
                          A list of (n_products - n_reactants)
                          for each reation
     """
+    free_energy_sorted = {}
+    #    gibs_energy_list = []
+    for i in species_list:
+        if chemkin:
+            free_energy_sorted[i] = free_energy[i][3]
+        else:
+            free_energy_sorted[i] = free_energy[i]
 
-    mol_change = []
-    gibbs_energy_list = []
-
-    for i, item in enumerate(complete_list):
-        n_reac = 0
-        n_prod = 0
-        reac_free_energy = 0
-        prod_free_energy = 0
-        for entry in item:
-
-            if float(entry[0]) < 0:
-                n_reac = n_reac + abs(float(entry[0]))
-                reac_free_energy = (abs(float(entry[0])) *
-                                    free_energy[entry[1]] +
-                                    reac_free_energy)
-            else:
-                prod_free_energy = (abs(float(entry[0])) *
-                                    free_energy[entry[1]]
-                                    + prod_free_energy)
-                n_prod = n_prod + abs(float(entry[0]))
-        # print(n_reac)
-        mol_change.append(n_prod - n_reac)
-        # print(mol_change)
-        gibbs_energy_list.append((prod_free_energy - reac_free_energy)
-                                 * HT_JL)
-
-    # equilibrium_constants = [np.exp(-n * 1000/(GAS_CONST * T))
-    #                         for n in gibbs_energy_list]
-    # reverse_rates = [(a / b) * 1000 * (GAS_CONST * T / PR_ATM) ** c
-    #                 if c < 3 else 0 for (a, b, c) in zip(forward_rates,
-    #                 equilibrium_constants, mol_change)]
+    mol_change = stoic_mat.sum(axis=1, dtype=float)
+    free_energy_list = list(free_energy_sorted.values())
+    gibbs_energy_list = np.dot(stoic_mat, free_energy_list) * factor
 
     return gibbs_energy_list, mol_change
 
 
-def build_kmatrix_reverse(complete_list, free_energy,
-                          forward_rates, temp):
+def build_kmatrix_reverse(free_energy, species_list, stoic_mat,
+                          factor, forward_rates, temp, chemkin=True):
     """"
     Calculates the reverse rate constants for all the reactions
     using the free energy change through the following steps
@@ -332,13 +547,20 @@ def build_kmatrix_reverse(complete_list, free_energy,
     total number of reactant molecules
     Parameters
     ----------
-    complete_list        : list
-                           A list of all the reactions with reactant and
-                         product species and their stoichimetric coeffs
     free_energy          : dict
                           A dictionary of free energies of all the species
                           at a given temperature, obtained from
-                          build_free_energy_dict function
+                          build_free_energy_dict function or from chemkin
+                          thermo files
+    species_list        : list
+                        a list of unique species in the mechanism
+    stoic_mat           : nampy array
+                        stoichiometric matrix of the mechanism
+    factor              : float
+                        conversion factor from given unit of energy to kJ
+    chemkin             : bool
+                        indicates if chemkin files are read as input files
+                        default = True
     forward_rates        : A list of forward rate constants for all the
                          reactions obtained from build_forward_reaction_rates
     temp                : float
@@ -349,8 +571,10 @@ def build_kmatrix_reverse(complete_list, free_energy,
                          A list of reverse rate constants
     """
 
-    gibbs_energy, change_mol = build_free_energy_change(complete_list,
-                                                        free_energy)
+    gibbs_energy, change_mol = build_free_energy_change(free_energy,
+                                                        species_list,
+                                                        stoic_mat, factor,
+                                                        chemkin)
 
     equilibrium_constants = [np.exp(-n * 1000/(GAS_CONST * temp))
                              for n in gibbs_energy]
@@ -426,37 +650,6 @@ def build_rate(reac_prod, spc_list, matrix, index, indices_species):
     rate_reac = rate_f + concentration_f + rate_r + concentration_r
 
     return rate_reac
-
-
-def build_stoic_matrix(complete_list, species_list, indices_species):
-    """
-    builds the stoichiometric matrix for the entire mechanism and
-    then builds the rate equations for each reaction.
-    Parameters
-    ----------
-    complete_list    : list
-                      A list of all the reactions with reactant and
-                      product species and their stoichimetric coeffs
-    species_list     : list
-                     A list of unique species in the mechanism
-    indices_species  : dict
-                     the dictionary species_indices
-    Returns
-    ----------
-    matrix           : matrix
-                     stoichiometric matrix for the entire
-                     reaction mechanism
-    rate_final       : str
-                     list of strings of rate equations for each
-                        reaction
-    """
-
-    matrix = np.zeros((len(complete_list), len(species_list)), dtype=float)
-    rate_final = []
-    for rxnindex, reac_list in enumerate(complete_list):
-        rate = build_rate(reac_list, species_list, matrix, rxnindex, indices_species)
-        rate_final.append(rate)
-    return matrix, rate_final
 
 
 def build_reaction_eqn(matrix, reaction_rate, rev_rate):

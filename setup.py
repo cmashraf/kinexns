6acb26a5de965f9e5905606ce4979c56f6bbdcda
Chowdhury Ashraf <cmashraf@uw.edu>
2019-08-30 12:10:05 -0700

    test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
 # Get version and release info, which is all stored in kmpy/version.py
-ver_file = os.path.join('kmpy', 'version.py')
+ver_file = os.path.join('kinexns', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
 

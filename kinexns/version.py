7a95afd442ea010cf8283556f6d063cc00643019
Chowdhury Ashraf <44684702+cmashraf@users.noreply.github.com>
2020-07-06 11:32:20 -0400

    Update version.py

--- kinexns/version.py
+++ kinexns/version.py
@@ -26,7 +26,7 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Topic :: Scientific/Engineering"]
 
 #  Description should be a one-liner:
-description = "shablona: a template for small scientific Python projects"
+description = "kinexns: Building and optimizing chemical kinetic models"
 #  Long description will go up on the pypi page
 long_description = """
 
@@ -40,27 +40,21 @@ more importantly, it contains infrastructure for testing, documentation,
 continuous integration and deployment, which can be easily adapted
 to use in other projects.
 
-To get started using these components in your own software, please go to the
-repository README_.
-
-.. _README: https://github.com/uwescience/shablona/blob/master/README.md
 
 License
 =======
-``shablona`` is licensed under the terms of the MIT license. See the file
+``kinexns`` is licensed under the terms of the MIT license. See the file
 "LICENSE" for information on the history of this software, terms & conditions
 for usage, and a DISCLAIMER OF ALL WARRANTIES.
 
 All trademarks referenced herein are property of their respective holders.
 
-Copyright (c) 2015--, Ariel Rokem, The University of Washington
-eScience Institute.
 """
 
 NAME = "kinexns"
 MAINTAINER = "Chowdhury Ashraf"
 MAINTAINER_EMAIL = "cmashraf@uw.edu"
-DESCRIPTION = "Building and optimizing kinetic models for bio-fuel pyrolysis"
+DESCRIPTION = "Building and optimizing chemical kinetic models"
 LONG_DESCRIPTION = long_description
 URL = "https://github.com/cmashraf/kinexns"
 DOWNLOAD_URL = ""
